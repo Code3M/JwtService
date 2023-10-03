e7d1e16cc55af282ac755dba31bab7efa20f8aac
Mahesh <maheshdasoffice9@gmail.com>
2023-10-03 13:44:51 +0530

    eureka server added

--- src/main/java/com/jwtservice/Code3M/JwtService/JwtServiceApplication.java
+++ src/main/java/com/jwtservice/Code3M/JwtService/JwtServiceApplication.java
@@ -2,10 +2,9 @@ package com.jwtservice.Code3M.JwtService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
-@EnableEurekaServer
 public class JwtServiceApplication {
 
 	public static void main(String[] args) {
